43f8744c52eb6c1477d433efe6b8c33af6e61814
chm2920 <chm2920@gmail.com>
2011-07-30 16:09:54 +0800

    update

--- app/controllers/news_topics_controller.rb
+++ app/controllers/news_topics_controller.rb
@@ -3,7 +3,7 @@ class NewsTopicsController < ApplicationController
   
   def index
     @topics = NewsTopic.paginate :page => params[:page], :per_page => 15, :conditions => "news_category_id = 1", :order => "id desc"
-    @page_title = "新闻"
+    @page_title = "青铜峡新闻"
   end
   
   def show
